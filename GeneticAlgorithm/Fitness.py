1a05bc71306b64df706bde6829d9fee8e2cdbf47
Cristian Grosu <cristian@codinglab.com>
2022-02-17 12:30:32 +0100

    added fitness functions

--- GeneticAlgorithm/Fitness.py
+++ GeneticAlgorithm/Fitness.py
@@ -6,14 +6,39 @@ d = 1
 def CO(x):
     return np.sum(x)
 
-def B(x):
+def B_1(x):
+    # d = 1
     co = CO(x)
     if co == k:
         return k
     else:
-        return k - d - (k-d)/(k-1) * co
+        return k - 1 - co
 
-def TF(x):
+def B_2(x):
+    # d = 2.5
+    co = CO(x)
+    if co == k:
+        return k
+    else:
+        return k - 2.5 - (k- 2.5)/(k-1) * co
+
+def TF_deceptive_linked(x):
     l = len(x)
     m = int(l/k)    
-    return np.sum([B(x[j*k : j*k + k]) for j in range(m)])
+    return np.sum([B_1(x[j*k : j*k + k]) for j in range(m)])
+
+
+def TF_non_deceptive_linked(x):
+    l = len(x)
+    m = int(l/k)    
+    return np.sum([B_2(x[j*k : j*k + k]) for j in range(m)])
+
+def TF_deceptive_not_linked(x):
+    l = len(x)
+    m = int(l/k)    
+    return np.sum([B_1(x[j : : k]) for j in range(k)])
+
+def TF_non_deceptive_not_linked(x):
+    l = len(x)
+    m = int(l/k)    
+    return np.sum([B_2(x[j : : k]) for j in range(k)])
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from GeneticAlgorithm.BitArrayGenome import BitArrayGenome
-from GeneticAlgorithm.Fitness import TF, CO
+from GeneticAlgorithm.Fitness import CO, TF_deceptive_linked, TF_deceptive_not_linked, TF_non_deceptive_linked, TF_non_deceptive_not_linked
 from GeneticAlgorithm.utils import find_optimal_population_size, test_ga
 
-GENOME_CONFIG = {'type': BitArrayGenome, 'size': 40, 'crossover': 'UX'}
+GENOME_CONFIG = {'type': BitArrayGenome, 'size': 40, 'crossover': '2X'}
 
 def main():   
-   print(find_optimal_population_size(TF, genome_config= GENOME_CONFIG))       
+   print(find_optimal_population_size(TF_non_deceptive_linked, genome_config= GENOME_CONFIG))       
    #test_ga(TF, genome_config= GENOME_CONFIG, population_size= 1280)
      
 if __name__ == '__main__':
