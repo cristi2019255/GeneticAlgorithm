c7003776d4745e5b4d468fc2cd3555d1fecacbbe
Cristian Grosu <cristian@codinglab.com>
2022-03-05 10:59:37 +0100

    added fitness correlation coefficient analysis

--- GeneticAlgorithm/utils.py
+++ GeneticAlgorithm/utils.py
@@ -1,3 +1,4 @@
+from random import shuffle
 import time
 from tqdm import tqdm
 from GeneticAlgorithm.GA import GeneticAlgorithm
@@ -119,3 +120,31 @@ def test_ga(fitness_function, genome_config, population_size, nr_of_runs = 20, p
                + '\n\n')                  
 
     print('Done ... look in the ' + RESULTS_FILE_NAME + ' for the results')
+    
+def find_crossover_fitness_correlation(fitness_function, genome_config, population_size):
+        correlation = []
+        ga = GeneticAlgorithm(pop_size = population_size, 
+                                        optimum= OPTIMAL_SOLUTION,
+                                        genome_config=genome_config,
+                                        fitness_function= fitness_function
+                                        )
+        parent_fitnesses = [ga.fitness(g.chromosome) for g in ga.population]
+        for _ in range(NUMBER_OF_RUNS):                                                    
+            new_population = []
+            for index in range(0, ga.pop_size, 2):            
+                parent1, parent2 = ga.population[index], ga.population[index + 1]            
+                child1, child2 = parent1.crossover(parent2)            
+                
+                # evaluating fitness                
+                new_population.append(ga.fitness(child1.chromosome))            
+                new_population.append(ga.fitness(child2.chromosome))
+        
+            children_fitnesses = new_population
+            
+            std1 = np.std(parent_fitnesses)
+            std2 = np.std(children_fitnesses)
+            m1 = np.mean(parent_fitnesses)
+            m2 = np.mean(children_fitnesses)
+            cov = sum([(parent_fitnesses[i] - m1) * (children_fitnesses[i] - m2) / population_size  for i in range(population_size)])            
+            correlation.append(cov / (std1 * std2))
+        return np.mean(correlation)    
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from GeneticAlgorithm.BitArrayGenome import BitArrayGenome
-from GeneticAlgorithm.Fitness import FITNESS_FUNCTIONS, CO, TF_deceptive_linked, TF_deceptive_not_linked, TF_non_deceptive_not_linked
-from GeneticAlgorithm.utils import find_optimal_population_size, test_ga
+from GeneticAlgorithm.Fitness import FITNESS_FUNCTIONS, CO
+from GeneticAlgorithm.utils import find_crossover_fitness_correlation, find_optimal_population_size, test_ga
 
 GENOME_CONFIG = {'type': BitArrayGenome, 'size': 40, 'crossover': '2X'}
 
@@ -26,9 +26,19 @@ def experiments():
    GENOME_CONFIG['crossover'] = 'UX'
    experiments_one_type_crossover()
 
+def experiments_fitness_correlation_coefficient():
+   population_size = 200
+   for fitness_function in FITNESS_FUNCTIONS:
+         GENOME_CONFIG['crossover'] = '2X'   
+         correlation_2x = str(round(find_crossover_fitness_correlation(fitness_function, GENOME_CONFIG, population_size), 4))   
+         GENOME_CONFIG['crossover'] = 'UX'   
+         correlation_ux = str(round(find_crossover_fitness_correlation(fitness_function, GENOME_CONFIG, population_size), 4))   
+         print(fitness_function.__name__.replace('_',' ') + ' & ' + correlation_2x + '& ' + correlation_ux + '\\\\')    
+
 def main():      
    #experiments()    
    test_ga(CO, genome_config= GENOME_CONFIG, population_size= 200, nr_of_runs=1, plot_results= True, trace_measures = True)        
-     
+   #experiments_fitness_correlation_coefficient()
+   
 if __name__ == '__main__':
    main()
